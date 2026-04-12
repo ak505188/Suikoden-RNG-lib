93854ec5d83a65c72a6552d0bc86bf1bb9fe8803
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2026-04-12 01:01:02 -0700

    Fix chinchironin add

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export { default as RNG } from './lib/rng.js';
 export { Areas } from './lib/lib.js';
 export { default as Helpers } from './lib/lib.js';
-export { Chinchironin } from
+export * as Chinchironin from './lib/chinchironin.js';

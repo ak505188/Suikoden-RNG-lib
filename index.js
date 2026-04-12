39b49f0a218d18978a8adae84e8e16f6416236d4
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2026-04-12 01:05:32 -0700

    Fix exports again?

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 export { default as RNG } from './lib/rng.js';
 export { Areas } from './lib/lib.js';
 export { default as Helpers } from './lib/lib.js';
-export * as Chinchironin from './lib/chinchironin.js';
+
+export {
+  CURSOR_POSITIONS,
+  Cursor,
+  calculateWait,
+  simulateRoll,
+  isValidTaiHoRoll,
+  simulateOpponentRollsFromGameStart,
+  simulateOpponentRoll,
+} from './lib/chinchironin.js';
